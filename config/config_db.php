4fc15ff7ce8d8f7d1cefa1cd8015fdde4dd09aa3
dtiguarulhos <ti.eflch@gmail.com>
2015-05-26 10:32:05 -0300

    versao 0.1

---
+++ config/config_db.php
@@ -0,0 +1,13 @@
+<?php
+ class DB extends DBmysql {
+                
+ var $dbhost = '172.21.160.19';
+                
+ var $dbuser 	= 'ocsguarulhos';
+                
+ var $dbpassword= 'asustuv4x';
+                
+ var $dbdefault	= 'glpiguarulhos';
+                
+ } 
+?>
